9f3ff083f55e6fe28ed28ed4d8dc29977496dfd9
Robin <robin.klass@gmx.de>
2021-12-08 19:30:21 +0100

    implement class

--- api/src/main/java/de/unistuttgart/iste/sopraws20/api/values/HistoricalValue.java
+++ api/src/main/java/de/unistuttgart/iste/sopraws20/api/values/HistoricalValue.java
@@ -1,5 +1,42 @@
 package de.unistuttgart.iste.sopraws20.api.values;
 
-public interface HistoricalValue {
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.litesoftwares.coingecko.domain.Coins.MarketChart;
+
+public class HistoricalValue {
+	@NotNull
+	@Size(min = 1)
+	private String name;
+	@NotNull
+	private MarketChart standardPriceChart;
+	@NotNull
+	private String description;
+
+	public HistoricalValue(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public MarketChart getStandardPriceChart() {
+		return standardPriceChart;
+	}
+
+	public void setStandardPriceChart(MarketChart standardPriceChart) {
+		this.standardPriceChart = standardPriceChart;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 }
