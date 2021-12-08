bdbca82a19b9a6a36870d9e33b608d13c556d650
Robin <robin.klass@gmx.de>
2021-12-08 19:29:45 +0100

    test wrapped coingecko java api access

--- api/src/main/java/de/unistuttgart/iste/sopraws20/api/Application.java
+++ api/src/main/java/de/unistuttgart/iste/sopraws20/api/Application.java
@@ -1,16 +1,27 @@
 package de.unistuttgart.iste.sopraws20.api;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.litesoftwares.coingecko.CoinGeckoApiClient;
+import com.litesoftwares.coingecko.domain.Coins.CoinList;
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
+
 @SpringBootApplication
 
 public class Application {
 
 	public static void main(String[] args) {
+		CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
+		// load list of coins
+		List<CoinList> coinList = client.getCoinList();
+		// private Marketclient.getCoinMarketChartById("bitcoin", "eur", 1);
+		System.out.println(coinList);
 		SpringApplication.run(Application.class, args);
 	}
 
@@ -20,9 +31,9 @@ public class Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				// allow CORS requests for all resources and HTTP methods from the frontend origin
-				registry.addMapping("/**")
-						.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE")
+				// allow CORS requests for all resources and HTTP methods from the frontend
+				// origin
+				registry.addMapping("/**").allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE")
 						.allowedOrigins("http://localhost:8000");
 			}
 		};
--- api/src/main/java/de/unistuttgart/iste/sopraws20/api/database/Importer.java
+++ api/src/main/java/de/unistuttgart/iste/sopraws20/api/database/Importer.java
@@ -1,5 +1,9 @@
 package de.unistuttgart.iste.sopraws20.api.database;
 
+/*
+ * will be implemented if more than one API is being used
+ * Standard format for Crypto-Data will be same as CoinGecko-API
+ */
 public class Importer {
 
 }
