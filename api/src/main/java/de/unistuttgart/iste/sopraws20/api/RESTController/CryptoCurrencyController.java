b4fd9e61c657499353d38c11afbbaf6a9db3aa49
Robin <robin.klass@gmx.de>
2021-12-09 16:40:43 +0100

    added basic REST resources and implemented GET for list of names

--- api/src/main/java/de/unistuttgart/iste/sopraws20/api/RESTController/CryptoCurrencyController.java
+++ api/src/main/java/de/unistuttgart/iste/sopraws20/api/RESTController/CryptoCurrencyController.java
@@ -3,18 +3,27 @@ package de.unistuttgart.iste.sopraws20.api.RESTController;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.litesoftwares.coingecko.exception.CoinGeckoApiException;
 import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
 
+import de.unistuttgart.iste.sopraws20.api.database.Importer;
 import de.unistuttgart.iste.sopraws20.api.values.CryptoCurrency;
 
 @RestController
 public class CryptoCurrencyController {
 
 	private List<CryptoCurrency> cryptoCurrencies;
+	// list of name or ticker symbol of cc
+	private List<String> cryptoCurrencyNames;
 
 	@PostConstruct
 	public void init() {
@@ -26,6 +35,45 @@ public class CryptoCurrencyController {
 			// TODO
 
 		}
+		// TODO: load list of cc
+	}
+
+	// gets list of Strings with crypto names
+	@GetMapping("/cryptos/list")
+	public List<String> getCryptoCurrencyNames() {
+		cryptoCurrencyNames = Importer.getCryptoCurrencyNames();
+
+		return cryptoCurrencyNames;
+		// TODO
+	}
+
+	// adds cc with name, autofills information
+	@PostMapping("/cryptos")
+	public CryptoCurrency addCryptoCurrency(@Valid @RequestBody String name) {
+		// TODO
+		return null;
+
+	}
+
+	@GetMapping("cryptos/{name}")
+	public CryptoCurrency getCryptoCurrencyByName(@PathVariable("name") String name) {
+		// TODO
+		return null;
+
+	}
+
+	@PostMapping("cryptos/{name}/{logourl}")
+	public String setLogoUrl(@PathVariable("name") String name, @PathVariable("logourl") String logoUrl) {
+		// TODO
+		return logoUrl;
+
+	}
+
+	@PutMapping("cryptos/{name}/{logourl}")
+	public String editLogoUrl(@PathVariable("name") String name, @PathVariable("logourl") String logoUrl) {
+		// TODO
+		return logoUrl;
+
 	}
 
 }
