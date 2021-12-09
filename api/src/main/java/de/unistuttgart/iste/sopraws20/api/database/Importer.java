843e0cb5803e0577ae157227eee982db932e29a4
Robin <robin.klass@gmx.de>
2021-12-09 16:39:34 +0100

    added coin name import

--- api/src/main/java/de/unistuttgart/iste/sopraws20/api/database/Importer.java
+++ api/src/main/java/de/unistuttgart/iste/sopraws20/api/database/Importer.java
@@ -1,9 +1,27 @@
 package de.unistuttgart.iste.sopraws20.api.database;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.litesoftwares.coingecko.CoinGeckoApiClient;
+import com.litesoftwares.coingecko.domain.Coins.CoinList;
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
+
 /*
- * will be implemented if more than one API is being used
+ * will be implemented further if more than one API is being used
  * Standard format for Crypto-Data will be same as CoinGecko-API
  */
-public class Importer {
+public final class Importer {
 
+	static CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
+
+	public static List<String> getCryptoCurrencyNames() {
+		// load list of coins
+		List<CoinList> coinLists = client.getCoinList();
+		List<String> coinNames = new LinkedList<String>();
+		for (CoinList coinList : coinLists) {
+			coinNames.add(coinList.getName());
+		}
+		return coinNames;
+	}
 }
