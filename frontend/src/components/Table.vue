85f6bb5f831dd4421d0dd1f020ab0f13656ec8d3
Yichi Zhang <yichizhang15@gmail.com>
2021-12-14 03:30:37 +0100

    Add Heat Map Example and vue multiselect for cryptho currency

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,37 +1,150 @@
 <template>
-    <!--Table is static, change to dynamic if we have data-->
-    <div class="table">
-        <table class="table table-hover">
-              <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
+    <div id="chart">
+        <apexcharts type="heatmap" height="350" :options="chartOptions" :series="series"></apexcharts>
     </div>
 </template>
 
 <script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+    name: 'HeatMap',
+    components: {
+        apexcharts: VueApexCharts
+    },
+    data: function() {
+          return{
+            series: [{
+              name: 'Jan',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Feb',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Mar',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Apr',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'May',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Jun',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Jul',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Aug',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            },
+            {
+              name: 'Sep',
+              data: this.generateData(20, {
+                min: -30,
+                max: 55
+              })
+            }
+          ],
+            chartOptions: {
+                chart: {
+                height: 350,
+                type: 'heatmap',
+                },
+                plotOptions: {
+                heatmap: {
+                    shadeIntensity: 0.5,
+                    radius: 0,
+                    useFillColorAsStroke: true,
+                    colorScale: {
+                    ranges: [{
+                        from: -30,
+                        to: 5,
+                        name: 'low',
+                        color: '#00A100'
+                        },
+                        {
+                        from: 6,
+                        to: 20,
+                        name: 'medium',
+                        color: '#128FD9'
+                        },
+                        {
+                        from: 21,
+                        to: 45,
+                        name: 'high',
+                        color: '#FFB200'
+                        },
+                        {
+                        from: 46,
+                        to: 55,
+                        name: 'extreme',
+                        color: '#FF0000'
+                        }
+                    ]
+                    }
+                }
+                },
+                dataLabels: {
+                enabled: false
+                },
+                stroke: {
+                width: 1
+                },
+                title: {
+                text: 'HeatMap Chart with Color Range'
+                },
+            },
+          }
+          
+        },
+        methods: {
+            generateData(count, yrange) {
+            var i = 0;
+            var series = [];
+            while (i < count) {
+                var x = (i + 1).toString();
+                var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+                series.push({
+                x: x,
+                y: y
+                });
+                i++;
+            }
+            return series;
+            }
+        }
+}
 </script>
--- frontend/src/views/Tables.vue
+++ frontend/src/views/Tables.vue
@@ -7,10 +7,15 @@
                         <b-dropdown-item>Time Frame</b-dropdown-item>
                         <b-dropdown-item>Currency</b-dropdown-item>
                     </b-dropdown><br>
-                    <b-dropdown block id="Courses" text="Courses" class="py-1">
-                        <b-dropdown-item>Bitcoin</b-dropdown-item>
-                        <b-dropdown-item>Etherium</b-dropdown-item>
-                    </b-dropdown><br>
+                    <label class="typo__label">Courses</label>
+                    <multiselect    v-model="value" 
+                                    label="name" 
+                                    track-by="name" 
+                                    :options="options" 
+                                    :multiple="true" 
+                                    :taggable="false" 
+                                    @tag="searchTag">
+                    </multiselect>
                     <b-button block>Export</b-button>
                 </b-container>
             </b-col>
@@ -22,13 +27,39 @@
 </template>
 
 <script>
-import Table from "@/components/Table.vue";
+import Table from "@/components/Table.vue"
+import Multiselect from 'vue-multiselect'
 
 export default {
     name: "Tables",
     components: {
-        Table
+        Table,
+        Multiselect
+    },
+  data () {
+    return {
+      value: null,
+      options: [
+            { id: "bitcoin", name: "Bitcoin"},
+            { id: "ethereum", name : "Ethereum"},
+            { id: "binancecoin", name: "Binance Coin"},
+            { id: "tether", name: "Tether"},
+            { id: "solana", name: "Solana"},
+            { id: "usd-coin", name: "USD Coin"},
+            { id: "cardano", name: "Cardano"},
+            { id: "ripple", name: "XRP"},
+            { id: "polkadot", name: "Polkadot"},
+            { id: "terra-luna", name: "Terra"}
+      ]
     }
+  },
+  methods: {
+    searchTag ({ name }) {
+      return `${name}`
+    }
+  }
 }
 
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
